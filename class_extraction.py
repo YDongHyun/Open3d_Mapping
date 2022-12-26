571622236b6bafaadd63498c646a7aeee9e78b32
양동현 <80799025+YDongHyun@users.noreply.github.com>
2022-12-26 20:04:59 +0900

    Update class_extraction.py

--- class_extraction.py
+++ class_extraction.py
@@ -5,10 +5,13 @@ import cv2
 import matplotlib.pyplot as plt
 
 img= cv2.imread("image/00005.png")
+img=cv2.resize(img,(212,120))
 dep = cv2.imread("depth/00005.png")
+dep=cv2.resize(dep,(212,120))
+
 
 def class_mask(img):
-    img=cv2.resize(img,(848,480))
+    #img=cv2.resize(img,(848,480))
     class_mask = np.copy(img)
     thresholds = (img[:,:,0] >250) & (img[:,:,1] >250) &(img[:,:,1]>250) # 기준 색상 필터링
     class_mask[thresholds] = [0]
@@ -20,9 +23,25 @@ def depth_mask(img,dep):
     depth_mask[thresholds] = [0]
     return depth_mask
 
-class_img=class_mask(img)
-depth_img=depth_mask(class_img,dep)
+def draw_poly(img):
+    gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    contours, hierarchy= cv2.findContours(gray, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+    for i in range(len(contours)):
+        print(i)
+        img = cv2.drawContours(img, [contours[i]], 0, (0,255,0), 2)
+        cv2.imshow("tset",img)
+    return img
 
+class_img=class_mask(img)
+
+depth_img=depth_mask(class_img,dep)
+#class_img=cv2.cvtColor(class_img,cv2.COLOR_BGR2HSV)
+#class_img=class_img*100/255
+#img=draw_poly(class_img)
+cv2.resize
+
+
+print(class_img)
 cv2.imshow("test",class_img)
 cv2.waitKey()
 
@@ -44,4 +63,10 @@ pcd = o3d.geometry.PointCloud.create_from_rgbd_image(
       rgbd_image,
       o3d.camera.PinholeCameraIntrinsic(
          o3d.camera.PinholeCameraIntrinsicParameters.PrimeSenseDefault))
-o3d.visualization.draw_geometries([pcd])
+
+#o3d.visualization.draw_geometries([pcd])
+hull, _ = pcd.compute_convex_hull()
+hull_ls = o3d.geometry.LineSet.create_from_triangle_mesh(hull)
+hull_ls.paint_uniform_color((1, 0, 0))
+o3d.visualization.draw_geometries([pcd, hull_ls])
+print(pcd)
